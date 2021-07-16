0766a27126c22db486aabfd8c58baae366489589
brendo <brendorutherlesdev@gmail.com>
2021-07-16 02:18:03 -0300

    add model boletim

--- src/models/boletim.model.js
+++ src/models/boletim.model.js
@@ -1,60 +1,86 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 const BoletimSchema = new mongoose.Schema(
   {
-    mike: {
-      type: String,
-      index: true
-    },
-    matricula: {
-      type: String,
-      index: true
-    },
-    responsavel: String,
-    tipoOcorr: String,
-    endereco: String,
-    date: Date,
-    hrregistro: String,
-    dataFato: Date,
-    descr_natureza: String,
-    gt: { type: String, default: "" },
-    efetivo: { type: String, default: "" },
+    mike: { type: String, default: " " },
+
+    matricula: { type: String, default: " " },
+
+    responsavel: { type: String, default: " " },
+    endereco: { type: String, default: " " },
+    bairro: { type: String, default: " " },
+    ponto: { type: String, default: " " },
+    municipio: { type: String, default: " " },
+    cep: { type: String, default: " " },
+    date: { type: String, default: " " },
+    horaRegistro: { type: String, default: " " },
+    dataFato: { type: String, default: " " },
+    descr_natureza: { type: String, default: " " },
+    tipoOcorr: { type: String, default: " " },
+    gt: { type: String, default: " " },
+    efetivo: { type: String, default: " " },
+    dataFato: { type: String, default: " "  },
+    horaFato: { type: String, default: " " },
+    pagina: { type: String, default: " " },
+
+
+
+
     pessoas: [
-    {
-        pai: String,
-        mae: String,
-        data_nascimento: String,
-        idade_aparente: String,
-        escolaridade: String,
-        endereco: String,
-        endereco_numero: String,
-        endereco_complemeneto: String,
-        naturalidade: { type: String, default: "" },
-        rg: { type: String, default: "" },
-        cpf: { type: String, default: "" },
-        telefone: { type: String, default: "" },
-        tipo: { type: String, default: "" },
+      {
+        pai: { type: String, default: " " },
+        nome: { type: String, default: " " },
+        mae: { type: String, default: " " },
+        idade_aparente: { type: String, default: " " },
+        escolaridade: { type: String, default: " " },
+        endereco: { type: String, default: " " },
+        endereco_numero: { type: String, default: " " },
+        endereco_complemeneto: { type: String, default: " " },
+        naturalidade: { type: String, default: " " },
+        rg: { type: String, default: " " },
+        cpf: { type: String, default: " " },
+        nascimento: { type: String, default: " " },
+        telefone: { type: String, default: " " },
+        tipo: { type: String, default: " " },
+        sexo: { type: String, default: " " },
       },
-    
-   ],
+    ],
+
+    historico: [
+      { 
+        historico: { type: String, default: " " } 
+      }],
+
+    veiculo: [
+      {
+        renavam: { type: String, default: " " },
+        ano: { type: String, default: " " },
+        cor: { type: String, default: " " },
+        placa: { type: String, default: " " },
+        envolvido: { type: String, default: " " },
+        chassi: { type: String, default: " " },
+        uf: { type: String, default: " " },
+        marca: { type: String, default: " " },
+      },
+    ],
+
     objetos: [
       {
-        tipo_objeto: { type: mongoose.Schema.ObjectId, ref: 'object_category' },
-        tipo_objeto_category: String,
-        qtd_objeto: String,
-        vlr_objeto: String,
-        envolvido: { type: String, default: "" },
-        modelo: { type: String, default: "" },
-        nserie: { type: String, default: "" },
-        histórico: { type: String, default: "" },
-      }
-    ]
+        tipo: { type: String, default: " " },
+        motivo: { type: String, default: " " },
+        qtd_objeto: { type: String, default: " " },
+        vlr_objeto: { type: String, default: " " },
+        envolvido: { type: String, default: " " },
+        modelo: { type: String, default: " " },
+        nserie: { type: String, default: " " },
+      },
+    ],
   },
   {
     timestamps: true,
-    strict: false // Novos conteúdos além dos que aqui especificados poderão ser adicionados
+    strict: false, // Novos conteúdos além dos que aqui especificados poderão ser adicionados
   }
-)
+);
 
-const Boletim = mongoose.model('boletim', BoletimSchema)
-export default Boletim
+const Boletim = mongoose.model("boletim", BoletimSchema);
+export default Boletim;
